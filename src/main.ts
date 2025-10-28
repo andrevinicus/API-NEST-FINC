e47ff4f87043c1af83c79ab63e33bfd03f1eb4d7
André Vinicius <andre.gorlin@gmail.com>
2025-10-28 11:18:44 -0300

    Verção inchuta

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
-// src/main.ts
 import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors(); // opcional se quiser testar via front-end ou Postman
-  await app.listen(process.env.PORT ?? 3004);
-  console.log(`🚀 Servidor rodando em http://localhost:${process.env.PORT ?? 3004}`);
+  app.enableCors();
+  const port = process.env.PORT ?? 3004;
+  await app.listen(port);
+  console.log(`🚀 Servidor rodando na porta ${port}`);
 }
 bootstrap();
